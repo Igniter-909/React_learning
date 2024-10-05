e8b64e3e69f7361f9be3751fcb45af656e6f4e08
Igniter-909 <igniter909505@gmail.com>
2024-10-05 19:37:48 +0530

    Added more files

---
+++ my-app/src/DogCard.js
@@ -0,0 +1,9 @@
+function DogCard(){
+    return (
+        <div className="DogCard">
+            <img src="my-app\dog.avif" alt="Dog"/>
+            <p>Bruno</p>
+        </div>
+    )
+}
+export default DogCard;
